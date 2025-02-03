0da8bed6837bd7fee0c6549b48b708e71afc4d0e
Parth Kotak <kotakparth44@gmail.com>
2025-02-03 15:18:46 -0800

    Updated google tag in App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Page3 from './pages/Page3';
 import Page4 from './pages/Page4';
 
 import ReactGA from 'react-ga4';
-ReactGA.initialize('G-55DMXVSCM9');
+ReactGA.initialize('G-PHMK2E102H');
 
 function App() {
   useEffect(() => {
